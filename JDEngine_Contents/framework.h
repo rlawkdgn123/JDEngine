d4a9bf58088ef59b06fc6202dd1970a979079bb0
rlawkdgn123 <63436456+rlawkdgn123@users.noreply.github.com>
2025-07-28 17:58:41 +0900

    contents pch 추가
    contents pch에
    #include "Global.h"
    #include "WindowGlobal.h"
    추가

--- JDEngine_Contents/framework.h
+++ JDEngine_Contents/framework.h
@@ -2,5 +2,7 @@
 
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
 
+#include "Global.h"
+#include "WindowGlobal.h"
 #include "GameObjectBase.h"
 #include "GameObjectList.h"
